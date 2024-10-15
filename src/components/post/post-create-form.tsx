15f22aac194dae73a4e505394525f01296764e13
jheo9 <jheo9@myseneca.ca>
2024-10-15 01:04:42 -0400

    2024-10-15

--- src/components/post/post-create-form.tsx
+++ src/components/post/post-create-form.tsx
@@ -12,7 +12,21 @@ import {
 import * as actions from '@/actions'
 import FormButton from "@/components/common/form-button"
 
-export default function PostCreateForm(){
+
+
+interface PostCreateFormProps{
+slug: string;
+
+}
+
+
+export default function PostCreateForm({slug}: PostCreateFormProps){
+    const [formState, action] = useFormState(
+        actions.createPost,
+        {
+            errors:{}
+        }
+    )
     return(
       <Popover placement="left">
         <PopoverTrigger>
@@ -27,17 +41,26 @@ export default function PostCreateForm(){
                     <h3 className="text-lg">Create a Post</h3>
 
                     <Input
+                    isInvalid={!!formState.errors.title}
+                    errorMessage ={formState.errors.title?.join(', ')}
                     name="title"
                     label ="Title"
                     labelPlacement="outside"
                     placeholder="Title" />
 
-                    <Input
+                    <Textarea
+                    isInvalid={!!formState.errors.content}
+                    errorMessage ={formState.errors.content?.join(', ')}
                     name = "content"
                     label = "Content"
                     labelPlacement="outside"
                     placeholder = "Content" />
-
+                    
+                    {formState.errors._form ? (
+                        <div className="rounded p-2 bg-red-200 border border-red-400">
+                            {formState.errors._form.join(', ')}
+                            </div>) : null
+                            }
                     <FormButton>
                         Save
                     </FormButton>
